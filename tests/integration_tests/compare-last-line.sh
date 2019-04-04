e1e38f21e38cb0653270689136ea547f20f58721
Juraj Hasik <jhasik@sissa.it>
2019-04-04 16:14:48 +0200

    tol 1e-5 for d=2 fu test

--- tests/integration_tests/compare-last-line.sh
+++ tests/integration_tests/compare-last-line.sh
@@ -20,15 +20,19 @@ then
     exit 1
 fi
 
-ll_out=`tail -n 1 $_out`
-ll_ref=`tail -n 1 $_ref`
+tail -n 1 $_out >> out_ll.dat.$$
+tail -n 1 $_ref >> ref_ll.dat.$$
 
-numdiff -a $_tol -r $_tol -s ' \t\n=,:;<>[](){}^' $ll_out $ll_ref
+numdiff -a $_tol -r $_tol -s ' \t\n=,:;<>[](){}^' out_ll.dat.$$ ref_ll.dat.$$
 
 # test if diff was successfull
 if [[ $? -ne 0 ]]
 then
     echo "diff failed"
+    rm out_ll.dat.$$
+    rm ref_ll.dat.$$
     exit 1
 fi
+rm out_ll.dat.$$
+rm ref_ll.dat.$$
 exit 0
